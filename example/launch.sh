935a4dd757de354e10a850f529486b886cc2195a
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-26 17:02:13 -0400

    [REF] Update sweep and launch script hyperparams

--- example/launch.sh
+++ example/launch.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-gpu=16G
-#SBATCH --time=00:06:00
+#SBATCH --time=00:04:00
 #SBATCH --qos=m5
-#SBATCH --array=0-2
-#SBATCH --signal=B:SIGUSR1@60  # Send signal SIGUSR1 60 seconds before the job hits the time limit
+#SBATCH --array=0-19
+#SBATCH --signal=B:SIGUSR1@120  # Send signal SIGUSR1 60 seconds before the job hits the time limit
 #SBATCH --open-mode=append
 
 echo "Job $SLURM_JOB_NAME ($SLURM_JOB_ID) begins on $(hostname), submitted from $SLURM_SUBMIT_HOST ($SLURM_CLUSTER_NAME)"
@@ -20,7 +20,7 @@ MAXWAIT=15
 sleep $((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
 
 # NOTE that we need to use srun here, otherwise the Python process won't receive the SIGUSR1 signal
-srun --unbuffered wandb agent --count=1 f-dangel-team/quickstart/evv70pcr &
+srun --unbuffered wandb agent --count=1 f-dangel-team/quickstart/i75puhon &
 child="$!"
 
 # Set up a handler to pass the SIGUSR1 to the python session launched by the agent
