1c3bc3221135555e3ccc3fa61f026dd28ba34147
Scott Lowe <scott.code.lowe@gmail.com>
2024-09-10 22:22:42 -0400

    [MNT] Don't overload os.path with lambda var

--- wandb_preempt/checkpointer.py
+++ wandb_preempt/checkpointer.py
@@ -266,7 +266,7 @@ class Checkpointer:
         """
         checkpoints_sorted = sorted(
             self.all_checkpoints(),
-            key=lambda path: int(path.split("/")[-1].replace(".pt", "").split("_")[-1]),
+            key=lambda x: int(x.split("/")[-1].replace(".pt", "").split("_")[-1]),
         )
         return checkpoints_sorted[-1] if checkpoints_sorted else None
 
